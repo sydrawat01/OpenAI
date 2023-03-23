8c41aff97a923c2ee6db30d71494f808ade51603
Siddharth Rawat <rawat.sid@northeastern.edu>
2023-03-23 10:52:01 -0400

    :fire_extinguisher: errorHandler middleware
    Includes a broad range of error handling. However, we need to handle bad
    requests only, because other errors are being handled by the GPT-3 API.
    
    Closes #2

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import cors from 'cors'
 
 import appConfig from './src/configs/app.config'
 import logger from './src/configs/logger.config'
+import { errorHandler } from './src/middlewares/errorHandler'
 import { healthRoute, askRoute } from './src/routes/index.routes'
 
 const app = express()
@@ -13,6 +14,7 @@ app.use(bodyParser.json())
 app.use(cors())
 
 app.use('/', healthRoute, askRoute)
+app.use(errorHandler)
 
 app.listen(PORT, () => {
   if (ENVIRONMENT !== 'prod')
